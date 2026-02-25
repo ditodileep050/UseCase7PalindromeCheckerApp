3dc8b2a2658e037a50971611fc04120d509664bb
ditodileep050 <dd9834@srmist.edu.in>
2026-02-25 09:43:07 +0530

    use case 7 palindrome checker

---
+++ src/UseCase7PalindromeCheckerApp.java
@@ -0,0 +1,38 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class UseCase7PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Define input string
+        String input = "refer";
+
+        // Create Deque
+        Deque<Character> deque = new ArrayDeque<>();
+
+        // Insert characters into deque
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        // Flag to track palindrome
+        boolean isPalindrome = true;
+
+        // Compare front and rear
+        while (deque.size() > 1) {
+
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
